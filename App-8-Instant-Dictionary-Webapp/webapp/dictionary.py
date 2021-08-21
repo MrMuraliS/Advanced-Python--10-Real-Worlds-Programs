6a9895417fc9d9fc6cfa72b2d1c78f5b0117c736
muralis <muralis1223@gmail.com>
2021-08-21 19:49:00 +0530

    commiting changes

--- App-8-Instant-Dictionary-Webapp/webapp/dictionary.py
+++ App-8-Instant-Dictionary-Webapp/webapp/dictionary.py
@@ -1,4 +1,5 @@
 import justpy as jp
+import definition
 
 
 class Dictionary:
@@ -13,15 +14,17 @@ class Dictionary:
                classes="text-lg")
 
         input_div = jp.Div(a=div, classes="gird gird-cols-2")
-        input_box = jp.Input(a=input_div, placeholder="Type in a word here...",
+
+        output_div = jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-40")
+        input_box = jp.Input(a=input_div, placeholder="Type in a word here...", outputdiv=output_div,
                              classes="m-2 bg-gray-50 border-2 border-gray-200 rounded w-64 "
                                      "focus:bg-white focus:outline-none "
                                      "focus:border-purple-500 py-2 px-4")
+        input_box.on('input', cls.get_definition)
 
-        output_div = jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-40")
-        jp.Button(a=input_div, text="Get Definition",
-                  classes="border-2 text-gray-600", click=cls.get_definition,
-                  outputdiv=output_div, inputbox=input_box)
+        # jp.Button(a=input_div, text="Get Definition",
+        #           classes="border-2 text-gray-600", click=cls.get_definition,
+        #           outputdiv=output_div, inputbox=input_box)
 
         print(cls, req)
 
@@ -29,4 +32,6 @@ class Dictionary:
 
     @staticmethod
     def get_definition(widget, msg):
-        widget.outputdiv.text = widget.inputbox.value
+        defined = definition.Definition(widget.inputbox.value).get()
+        widget.outputdiv.text = " ".join(defined)
+        print(defined)
