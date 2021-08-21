3069e8dab18a074d08aee9e84a4d0b5886ce4c9f
muralis <muralis1223@gmail.com>
2021-08-21 20:33:41 +0530

    Committing API

--- App-9-Instant-Dictionary-API/api.py
+++ App-9-Instant-Dictionary-API/api.py
@@ -2,22 +2,26 @@ import justpy as jp
 import definition
 import json
 
+
 class Api:
-    """Handles requests at /api?w=word
+    """Handles requests at http://127.0.0.1:8000/?q=moon
     """
+
     @classmethod
     def serve(cls, req):
         wp = jp.WebPage()
-        word = req.query_params.get('w')
+        word = req.query_params.get('q')
 
         defined = definition.Definition(word).get()
 
         response = {
-            "word":word,
-            "definition":defined
+            "word": word,
+            "definition": defined
         }
 
         wp.html = json.dumps(response)
         return wp
 
 
+jp.Route("/", Api.serve)
+jp.justpy()
